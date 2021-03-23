076a37f26db7906433828af7b1ec43ba049f88da
Ruslan Kulibaev <ruslan0809.89@mail.ru>
2021-03-23 10:44:43 +0300

    task/FAT-12800-create-weatherapp-project
    Done: AppIcons, LaunchScreen, Images for UI

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Руслан Кулийбаев on 22.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
