7a88bc6e27fc8c16a40b1f18d828f32df281da5c
Ruslan Kulibaev <ruslan0809.89@mail.ru>
2021-03-23 18:52:00 +0300

    Revert "ViewController.swift"
    This reverts commit 3d6229295fa94ddb2b533bed9b8c67fde974b274.

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -1,8 +1,1 @@
-import UIKit
 
- class ViewController: UIViewController {
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         // Do any additional setup after loading the view.
-     }
- }
